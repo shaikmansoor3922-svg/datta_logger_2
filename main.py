3c4a0f72919911d62d5fe1b88a11e74d4b4257a9
shaikmansoor3922-svg <shaikmansoor3922@gmail.com>
2026-02-14 00:16:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ from fastapi import Request
 import time
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory="templates")
 
 latest_data = []
-last_update_time = 0
 all_data = []
+last_update_time = 0
 
 class SensorData(BaseModel):
     values: List[float]
@@ -21,23 +20,37 @@ class SensorData(BaseModel):
 def dashboard(request: Request):
     return templates.TemplateResponse("dashboard.html", {"request": request})
 
+
+# ESP POSTS HERE
 @app.post("/upload")
 def upload_data(data: SensorData):
-    global latest_data, last_update_time, all_data
-    
+    global latest_data, all_data, last_update_time
+
     latest_data = data.values
-    last_update_time = time.time()
     all_data.append(data.values)
+    last_update_time = time.time()
 
     return {"message": "Data received"}
 
+
+# Dashboard fetches live data
 @app.get("/latest")
 def get_latest():
-    return {
-        "values": latest_data,
-        "last_update": last_update_time
-    }
+    return {"values": latest_data}
 
-@app.get("/all")
-def get_all():
+
+# Dashboard fetches full history
+@app.get("/history")
+def get_history():
     return {"history": all_data}
+
+
+# Dashboard checks connection
+@app.get("/status")
+def get_status():
+    current_time = time.time()
+
+    if current_time - last_update_time < 3:
+        return {"device": "connected"}
+    else:
+        return {"device": "disconnected"}
