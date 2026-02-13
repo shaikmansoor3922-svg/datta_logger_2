940fe1975040b2ed62fb0b5b85b2eabecff66f3c
shaikmansoor3922-svg <shaikmansoor3922@gmail.com>
2026-02-14 00:12:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,43 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
+import time
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
-# Store all historical data
-all_data = []
 latest_data = []
+last_update_time = 0
+all_data = []
 
 class SensorData(BaseModel):
     values: List[float]
 
-# Dashboard page
 @app.get("/", response_class=HTMLResponse)
 def dashboard(request: Request):
     return templates.TemplateResponse("dashboard.html", {"request": request})
 
-# Upload data from ESP
 @app.post("/upload")
 def upload_data(data: SensorData):
-    global latest_data, all_data
+    global latest_data, last_update_time, all_data
+    
     latest_data = data.values
+    last_update_time = time.time()
     all_data.append(data.values)
-    print("Received:", latest_data)
-    return {"message": "Data received successfully"}
 
-# Latest values (for live dashboard)
+    return {"message": "Data received"}
+
 @app.get("/latest")
 def get_latest():
-    return {"values": latest_data}
+    return {
+        "values": latest_data,
+        "last_update": last_update_time
+    }
 
-# All historical values (for table button)
-@app.get("/history")
-def get_history():
-    return {"data": all_data}
+@app.get("/all")
+def get_all():
+    return {"history": all_data}
