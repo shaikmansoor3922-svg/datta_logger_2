f06fb228fe584e7ee7e0e36c4fff0e3b127c5c06
shaikmansoor3922-svg <shaikmansoor3922@gmail.com>
2026-02-13 22:29:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
 from fastapi.responses import HTMLResponse
+import os
 
 app = FastAPI()
 
@@ -10,9 +11,6 @@ latest_data = []
 class SensorData(BaseModel):
     values: List[float]
 
-@app.get("/")
-def home():
-    return {"status": "Server Running"}
 
 @app.post("/upload")
 def upload_data(data: SensorData):
@@ -21,28 +19,13 @@ def upload_data(data: SensorData):
     print("Received:", latest_data)
     return {"message": "Data received successfully"}
 
+
 @app.get("/latest")
 def get_latest():
     return {"values": latest_data}
 
-@app.get("/dashboard", response_class=HTMLResponse)
+
+@app.get("/", response_class=HTMLResponse)
 def dashboard():
-    return """
-    <html>
-    <head>
-        <title>ESP Data Dashboard</title>
-        <script>
-            async function fetchData() {
-                const res = await fetch('/latest');
-                const data = await res.json();
-                document.getElementById("data").innerText = JSON.stringify(data.values);
-            }
-            setInterval(fetchData, 2000);
-        </script>
-    </head>
-    <body onload="fetchData()">
-        <h2>Live Sensor Data</h2>
-        <p id="data">Loading...</p>
-    </body>
-    </html>
-    """
+    with open("dashboard.html", "r") as f:
+        return f.read()
